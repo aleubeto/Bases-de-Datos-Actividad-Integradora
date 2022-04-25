5898ea931e7fda189d0977c8e68e2354673255a0
ale_local <alejo.ubeto@gmail.com>
2022-04-25 18:36:06 -0500

    Elementos de index en <li>

--- index.php
+++ index.php
@@ -4,10 +4,12 @@
         <!--Head de la página de inicio-->
     </head>
     <body>
-        <a href="pag1_autos/pag1_index.php">Autos</a>
-        <a href="pag2_diaDelPeque/pag2_index.php">Día del peque</a>
-        <a href="pag3_chatBot/pag3_index.php">ChatBot: Ikta'an</a>
-        <a href="pag4_transacciones/pag4_index.php">Transacciones</a>
+        <ul>
+            <li><a href="pag1_autos/pag1_index.php">Autos</a></li>
+            <li><a href="pag2_diaDelPeque/pag2_index.php">Día del peque</a></li>
+            <li><a href="pag3_chatBot/pag3_index.php">ChatBot: Ikta'an</a></li>
+            <li><a href="pag4_transacciones/pag4_index.php">Transacciones</a></li>
+        </ul>
         <!--Body de la página de inicio-->
     </body>
 </html>
