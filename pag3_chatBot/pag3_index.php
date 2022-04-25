0c1f9935e4f2244af7cc97394bc3b5358b0b3d61
ale_local <alejo.ubeto@gmail.com>
2022-04-25 17:50:19 -0500

    Adición de títulos para páginas

---
+++ pag3_chatBot/pag3_index.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+    <head>
+        <!--Head de la página de ChatBot-->
+    </head>
+    <body>
+        <h1>ChatBot: Ikta'an</h1>
+    </body>
+</html>
---
+++ pag4_transacciones/pag4_index.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+    <head>
+        <!--Head de la página de Transacciones-->
+    </head>
+    <body>
+        <h1>Transacciones</h1>
+    </body>
+</html>
