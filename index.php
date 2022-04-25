7782778accb87fa42fe1e87a51663faf6d5b95a7
ale_local <alejo.ubeto@gmail.com>
2022-04-25 17:12:14 -0500

    Corrección de referencias de index.php

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
         <!--Head de la página de inicio-->
     </head>
     <body>
-        <a href="php/pag1_autos/pag1_index.php">Autos</a>
-        <a href="php/pag2_diaDelPeque/pag2_index.php">Día del peque</a>
-        <a href="php/pag4_transacciones/pag4_index.php">Transacciones</a>
+        <a href="pag1_autos/pag1_index.php">Autos</a>
+        <a href="pag2_diaDelPeque/pag2_index.php">Día del peque</a>
+        <a href="pag4_transacciones/pag4_index.php">Transacciones</a>
         <!--Body de la página de inicio-->
     </body>
 </html>
