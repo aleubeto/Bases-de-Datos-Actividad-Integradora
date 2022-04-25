d39438923b8403df439d2a483fd9d41de82d570b
ale_local <alejo.ubeto@gmail.com>
2022-04-25 17:01:53 -0500

    Creación de referencias a páginas desde index.php

--- index.php
+++ index.php
@@ -4,8 +4,9 @@
         <!--Head de la página de inicio-->
     </head>
     <body>
-        <p>Prueba</p>
+        <a href="php/autos/index.php">Autos</a>
+        <a href="php/pag2_diaDelNino.php">Día del peque</a>
+        <a href="php/pag4_transacciones.php">Transacciones</a>
         <!--Body de la página de inicio-->
-        <!--Comentario de prueba, exito!-->
     </body>
 </html>
