cdf4081ad39c64403416c0158924b2eb894e6483
ale_local <alejo.ubeto@gmail.com>
2022-04-26 00:02:29 -0500

    Correción de index

--- index.php
+++ index.php
@@ -7,10 +7,11 @@
     </style>
     <head>
         <link rel="stilones" href="stilo.css">
+        <title>Inicio</title>
     </head>
     <body>
         <ul>
-            <a href="pag1_autos/pag1_index.php"><img src="images/masterchef.jpeg" alt="El wen master chef" width="150" height="220"> </a>
+            <a href="pag1_autos/pag1_index.php">CRUD de autos</a>
             <li><a href="pag2_diaDelPeque/pag2_index.php">Día del peque</a></li>
             <li><a href="pag3_chatBot/pag3_index.php">ChatBot: Ikta'an</a></li>
             <li><a href="pag4_transacciones/pag4_index.php">Transacciones</a></li>
--- pag2_diaDelPeque/pag2_index.php
+++ pag2_diaDelPeque/pag2_index.php
@@ -5,7 +5,6 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Día del peque</title>
-<title>Register</title>
     <style>
       body {
         margin-top: 110px;
