21b7bf7136210ae6cfd0143affaf84552167b5c1
HaikerMaiker <a00830026@tec.mx>
2022-04-25 23:37:05 -0500

    Creo que ya jala el css

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <!doctype html>
 <html>
     <head>
-        <link rel="stilones" href="stilos.css">
+        <link rel="stilones" href="stilo.css">
     </head>
     <body>
         <ul>
-            <li><a href="pag1_autos/pag1_index.php"><img src="images/masterchef.jpeg" alt="El wen master chef" width="500" height="600"> </a></li>
+            <a href="pag1_autos/pag1_index.php"><img src="images/masterchef.jpeg" alt="El wen master chef" width="150" height="220"> </a>
             <li><a href="pag2_diaDelPeque/pag2_index.php">Día del peque</a></li>
             <li><a href="pag3_chatBot/pag3_index.php">ChatBot: Ikta'an</a></li>
             <li><a href="pag4_transacciones/pag4_index.php">Transacciones</a></li>
