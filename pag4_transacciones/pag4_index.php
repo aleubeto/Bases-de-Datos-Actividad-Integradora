e98c571e73437f2a3d7932edbbca560fe2dd26db
ale_local <alejo.ubeto@gmail.com>
2022-04-25 23:58:41 -0500

    Retoque de <head>'s

--- pag4_transacciones/pag4_index.php
+++ pag4_transacciones/pag4_index.php
@@ -1,10 +1,13 @@
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<html lang="en">
     <head>
-        <!--Head de la página de Transacciones-->
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Transacciones entre usuarios</title>
     </head>
     <body>
-        <h1>Transacciones</h1>
+        <h1>Transacciones entre usuarios</h1>
         <?php
             #código php
         ?>
