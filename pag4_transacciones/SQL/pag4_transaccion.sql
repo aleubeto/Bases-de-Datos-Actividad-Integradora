c697d054f89e34aa949e1a2ab2359997d054431f
ale_local <alejo.ubeto@gmail.com>
2022-04-25 23:47:47 -0500

    Creación de tablas de pag4

--- pag4_transacciones/SQL/pag4_transaccion.sql
+++ pag4_transacciones/SQL/pag4_transaccion.sql
@@ -1,6 +1,14 @@
 CREATE TABLE IF NOT EXISTS pag4_transaccion (
-  
+    idTransaccion INT(11) NOT NULL AUTO_INCREMENT,
+    monto INT(11) NOT NULL,
+    fecha DATETIME DEFAULT NULL,
+    idRemitente INT(11) NOT NULL,
+    idDestinatario INT(11) NOT NULL,
+    PRIMARY KEY(idTransaccion),
+    FOREIGN KEY(idRemitente)
+        REFERENCES pag4_usuario(idUsuario)
+        ON UPDATE CASCADE ON DELETE RESTRICT,
+    FOREIGN KEY(idDestinatario)
+        REFERENCES pag4_usuario(idUsuario)
+        ON UPDATE CASCADE ON DELETE RESTRICT,
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
-
-INSERT INTO pag4_transaccion () VALUES
-();
--- pag4_transacciones/SQL/pag4_usuario.sql
+++ pag4_transacciones/SQL/pag4_usuario.sql
@@ -1,6 +1,13 @@
 CREATE TABLE IF NOT EXISTS pag4_usuario (
-
+    idUsuario INT(11) NOT NULL AUTO_INCREMENT,
+    usuario VARCHAR(100) DEFAULT NULL,
+    saldo INT(11) NOT NULL,
+    PRIMARY KEY(idUsuario)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
 
-INSERT INTO pag4_usuario () VALUES
-();
+INSERT INTO pag4_usuario (usuario, saldo) VALUES
+("Alejandro Ubeto", 1000),
+("Iker Guerero", 1500),
+("Oscar Ramirez", 1250),
+("Francisco Rocha", 1000),
+("Juan Carlos Llanos", 2000);
