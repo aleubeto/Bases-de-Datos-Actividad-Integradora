5800b7bb5cdca8f001c63750d26e3947a7d3c6dc
ale-O-local <alejo.ubeto@gmail.com>
2022-05-02 21:01:57 -0500

    Creación de estructura html de transacciones

--- pag4_transacciones/pag4_index.php
+++ pag4_transacciones/pag4_index.php
@@ -1,60 +1,78 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-	    <meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	    <link href="css/bootstrap.min.css" rel="stylesheet">
-	    <script src="js/bootstrap.min.js"></script>
-		<title>CRUD de autos</title>
-	</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Transacciones</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <h2>Transferencias entre usuarios</h2>
+            </div>
+            <div class="row">
+                <table class="table table-striped table-bordered">
+                    <thead>
+                        <tr>
+                            <th>id</th>
+                            <th>nombre</th>
+                            <th>saldo</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+			<form action="form-horizontal" method="post">
+				<!--Remitente-->
+				<div class="control-group">
+					<label class="control-label">Remitente</label>
+					<div class="controls">
+						<select name="idRemitente">
+							<option value="">Selecciona un Remitente</option>
+							<?php
 
-	<body>
-	    <div class="container">
+							?>
+						</select>
+						<?php  ?>
+						<span class="help-inline"><?php  ?></span>
+					</div>
+				</div>
 
-    		<div class="row">
-    			<h3>Ejemplo de operaciones básicas a una tabla de autos</h3>
-    		</div>
+				<!--Destinatario-->
+				<div class="control-group">
+					<label class="control-label">Destinatario</label>
+					<div class="controls">
+						<select name="idRemitente">
+							<option value="">Selecciona un Destinatario</option>
+							<?php
+							?>
+						</select>
+						<?php   ?>
+						<span class="help-inline"><?php  ?></span>
+					</div>
+				</div>
 
-			<div class="row">
-				<p>
-					<a href="create.php" class="btn btn-success">Agregar un Auto</a>
-				</p>
+				<!--Monto-->
+				<div class="control-group" <?php  ?>>
+					<label class="control-label">Monto</label>
+					<div class="controls">
+						<input name="monto" type="number" step="0.5" value="<?php   ?>">
+						<?php  ?>
+						<span class="help-inline">
+							<?php   ?>
+						</span>
+					</div>
+				</div>
 
-				<table class="table table-striped table-bordered">
-		            <thead>
-		                <tr>
-		                	<th>submarcamarca	</th>
-		                	<th>marca 			</th>
-                        	<th>A/C 			</th>
-		                </tr>
-		            </thead>
-		            <tbody>
-		              	<?php
-					   	include 'database.php';
-					   	$pdo = Database::connect();
-					   	$sql = 'SELECT * FROM pag1_auto natural join pag1_marca ORDER BY idauto';
-	 				   	foreach ($pdo->query($sql) as $row) {
-							echo '<tr>';
-    					   	echo '<td>'. $row['nombrec'] . '</td>';
-    					  	echo '<td>'. $row['nombrem'] . '</td>';
-                            echo '<td>';    echo ($row['ac'])?"SI":"NO"; echo'</td>';
-                            echo '<td width=250>';
-    					   	echo '<a class="btn" href="read.php?id='.$row['idauto'].'">Detalles</a>';
-    					   	echo '&nbsp;';
-    					  	echo '<a class="btn btn-success" href="update.php?id='.$row['idauto'].'">Actualizar</a>';
-    					   	echo '&nbsp;';
-    					   	echo '<a class="btn btn-danger" href="delete.php?id='.$row['idauto'].'">Eliminar</a>';
-    					   	echo '</td>';
-						  	echo '</tr>';
-					    }
-					   	Database::disconnect();
-					  	?>
-				    </tbody>
-	            </table>
-
-	    	</div>
-
-	    </div> <!-- /container -->
-	</body>
+				<!--Envíar formulario-->
+				<div class="form-actions">
+					<button type="submit" class="btn btn-success">Transferir</button>
+				</div>
+			</form>
+        </div>
+    </body>
 </html>
