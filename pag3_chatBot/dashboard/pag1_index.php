2a5e5ab4316a2abccb324ae043e5025354fade63
juanllanos19 <o.llanos.juan19@gmail.com>
2022-05-06 01:14:27 -0500

    a

--- pag3_chatBot/dashboard/pag1_index.php
+++ pag3_chatBot/dashboard/pag1_index.php
@@ -6,27 +6,27 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	    <link href="css/bootstrap.min.css" rel="stylesheet">
 	    <script src="js/bootstrap.min.js"></script>
-		<title>CRUD de autos</title>
+		<title>Dashboard ChatBot</title>
 	</head>
 
 	<body>
 	    <div class="container">
 
     		<div class="row">
-    			<h3>Ejemplo de operaciones básicas a una tabla de autos</h3>
+    			<h3>Dashboard de preguntas y respuestas</h3>
     		</div>
 
 			<div class="row">
 				<p>
-					<a href="create.php" class="btn btn-success">Agregar un Auto</a>
+					<a href="create.php" class="btn btn-success">Agregar Pregunta</a>
 				</p>
 
 				<table class="table table-striped table-bordered">
 		            <thead>
 		                <tr>
-		                	<th>submarcamarca	</th>
-		                	<th>marca 			</th>
-                        	<th>A/C 			</th>
+		                	<th>Pregunta	</th>
+		                	<th>Respuesta 			</th>
+                        	<th>Categoria 			</th>
 		                </tr>
 		            </thead>
 		            <tbody>
